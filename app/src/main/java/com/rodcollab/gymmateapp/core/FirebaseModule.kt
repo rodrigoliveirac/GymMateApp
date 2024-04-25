41d2c61ec69ccae0712f126b96662a17868c50c3
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-04-25 18:31:37 -0300

    [feat-auth] - creating Firebase Module

---
+++ app/src/main/java/com/rodcollab/gymmateapp/core/FirebaseModule.kt
@@ -0,0 +1,21 @@
+package com.rodcollab.gymmateapp.core
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth() : FirebaseAuth {
+        return Firebase.auth
+    }
+}
